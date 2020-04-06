4103720a12b5baa9e76dda0ecd552450999d759a
latarho <latarho@gmail.com>
2020-04-06 21:53:23 +0500

    Learning Python!

---
+++ homework_part_1_chapter_3.py
@@ -0,0 +1,10 @@
+print('Hello world!')
+
+# a = 2 ** 500
+# print(a)
+# b = 1 / 0
+# print(b)
+
+L = [1, 2]
+L.append(L)
+print(L)
---
+++ script1.py
@@ -0,0 +1,9 @@
+# Первый сценарий Python
+
+import sys
+print(sys.platform)
+print(2 ** 100)
+x = 'Spam!'
+print(x * 8)
+
+input()
