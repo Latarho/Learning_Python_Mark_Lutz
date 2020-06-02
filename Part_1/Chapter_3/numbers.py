0676b0023649a537855af965eaabf406d7e82e40
latar <Sambist1488!>
2020-06-02 11:28:28 +0500

    Chapter 4 complete

---
+++ Part_1/Chapter_3/numbers.py
@@ -0,0 +1,27 @@
+import math
+import random
+
+print(len(str(2 ** 1000)))
+
+x = math.sqrt(49)
+print(x)
+y = math.pi
+print(y)
+z = random.random()
+print(z)
+a = random.choice([1, 2, 3, 4, 5, 6, 7])
+print(a)
+
+# Strings
+b = 'One two free four five six seven.'
+# Количество символов в строке
+print(len(b))
+# Первый и второй элемент с начала
+print(b[0]), print(b[1])
+# Первый элемент с конца
+print(b[-1])
+
+S = 'cheburashka'
+G = list(S)
+print(G)
+
---
+++ Part_2/Chapter_4/Numbers.py
@@ -0,0 +1,16 @@
+# Целочисленное сложение
+print(123 + 222)
+# Умножение с плавающей точкой
+print(1.5 * 4)
+# 2 в степени 100
+print(2 ** 100)
+# Сколько цифр в большом числе?
+print(len(str(2 ** 100)))
+
+import math
+print(math.pi)
+print(math.sqrt(100))
+
+import random
+print(random.random())
+print(random.choice([1, 2, 3, 4, 5]))
